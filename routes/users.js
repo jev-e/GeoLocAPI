68d3459c92626b0f4f436d84ca7055b3c796e343
Will Kearney <wkearney@colby.edu>
2021-11-27 19:41:55 +0000

    Checks if user account already exists

--- routes/users.js
+++ routes/users.js
@@ -22,17 +22,13 @@ router.post('/login', function (req, res) {
         bcrypt.compare(req.body.password, user.password, (err, results) => {
           if (results) {
             res.status(200).send("User Authenticated");
-            
           } else {
             res.status(401).send("Password Incorrect");
           }
         })
       }
-
-
     })
-
-})
+});
 
 router.post('/create', function (req, res) {
   bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
@@ -43,24 +39,29 @@ router.post('/create', function (req, res) {
       creationDate: new Date(),
       password: hash,
     };
-    poolConnection.getDb()
-      .collection(process.env.MONGOUSERCOLLECTION)
-      .insertOne(userProfile, (err, results) => {
-        if (err) {
-          res.status(400).send("Error During User Creation");
 
-
+    // first make sure a user with that email doesn't already exist
+    poolConnection.getDb().collection(process.env.MONGOUSERCOLLECTION)
+      .findOne({
+        "userEmail": req.body.userEmail
+      }).then((user) => {
+        if (user) {
+          // account already exists with that user email! Return an error response
+          res.status(400).status("Account already exists with that email.");
         } else {
-          res.status(201).send("Created User With Username " + userProfile.userEmail)
-        }
-      })
-
-  })
-
-})
-
-
-
+          // otherwise, create a new user account
+          poolConnection.getDb().collection(process.env.MONGOUSERCOLLECTION)
+            .insertOne(userProfile, (err, results) => {
+              if (err) {
+                res.status(400).send("Error during user creation");
+              } else {
+                res.status(201).send("Created user with email " + userProfile.userEmail)
+              }
+            }) // end insertOne()
+        } // end else statement
+      }) // end then
+  }) // end hash()
+}); // end router.post()
 
 
 module.exports = router;
