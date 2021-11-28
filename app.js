b232d8965c773dfc5594cc5019f9bd72dd79a48c
Will Kearney <wkearney@colby.edu>
2021-11-28 21:08:18 +0000

    Debugging

--- app.js
+++ app.js
@@ -30,8 +30,6 @@ app.use('/api', apiRouter)
 //Connect to Mongo Cluster
 poolConnection.dbCon();
 
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
